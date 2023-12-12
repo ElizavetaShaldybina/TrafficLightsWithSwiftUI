fb319faca7170623684dd8cfcb04978de8545cc3
Elizaveta <elizavetash0210@mail.ru>
2023-12-12 20:51:42 +0300

    Initial Commit

---
+++ TrafficLightsWithSwiftUI/TrafficLightsWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsWithSwiftUIApp.swift
+//  TrafficLightsWithSwiftUI
+//
+//  Created by Елизавета Шалдыбина on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
